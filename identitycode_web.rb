882dbdb9ab33748246d930d26474b8d7494a116a
Artem Pakk <apakk@me.com>
2017-03-05 18:19:22 +0200

    Add identity code generation

--- identitycode_web.rb
+++ identitycode_web.rb
@@ -1,7 +1,39 @@
 require 'sinatra/base'
+require 'identity_code'
+require 'json'
 
 class IdentityCodeWeb < Sinatra::Base
   get '/' do
     haml :index
+  end
+
+  post '/generate' do
+    content_type :json
+
+    count = case params[:count].to_i
+    when proc { |n| n <= 0 }
+      1
+    when proc { |n| n > 100 }
+      100
+    else
+      params[:count].to_i
+    end
+
+    opts = {
+      count: count,
+      country: params[:country] || 'ee',
+      safe_age: params[:safe_age].nil? ? false : true,
+      sex: params[:sex]
+    }
+
+    codes = []
+    opts[:count].times do
+      codes << IdentityCode.generate(opts)
+    end
+
+    {
+      country: opts[:country],
+      result: codes
+    }.to_json
   end
 end
